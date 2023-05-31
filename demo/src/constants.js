25f38b728d6a026641e18dad0bc735e9070951ba
Seth Rider <seth@ridersblock.net>
2023-05-31 19:59:25 +0100

    package updates

--- demo/src/constants.js
+++ demo/src/constants.js
@@ -1,5 +1,5 @@
 // This is the URL of the service running the Hyperview examples
-export const ENTRY_POINT_URL = 'http://0.0.0.0:8085/index.xml';
+export const ENTRY_POINT_URL = 'http://192.168.1.119:8085/index.xml';
 
 export const MAIN_STACK_NAME = 'Main';
 
